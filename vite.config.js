96f5e4a9cae07f7620b76b96615d03174d4cbb96
kaganlove <98985059+kaganlove@users.noreply.github.com>
2025-11-16 14:12:26 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/chronostudy/',   // must match your repo name
   plugins: [react()],
 })
